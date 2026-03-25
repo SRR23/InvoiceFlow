c0377e7a7201a51cb0ed5f280e092a9dc918a292
SRR23 <srrimon267@gmail.com>
2026-03-25 21:37:00 +0600

    added

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -5,7 +5,7 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    """Admin configuration for User model."""
+   
     list_display = ('email', 'first_name', 'last_name', 'company_name', 'is_business_user', 'is_staff', 'is_active', 'date_joined')
     list_filter = ('is_business_user', 'is_staff', 'is_active', 'date_joined')
     search_fields = ('email', 'first_name', 'last_name', 'company_name')
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,6 +1,4 @@
-"""
-User model and authentication-related models.
-"""
+
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils import timezone
